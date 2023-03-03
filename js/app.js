0e62eca62395c7c0667ee88cc7cdc67d4e711a5c
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-03-03 09:50:28 +0600

    partial data showing complete

--- js/app.js
+++ js/app.js
@@ -3,29 +3,47 @@
 const loadData = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools')
         .then(res => res.json())
-        .then(data => representData(data.data.tools))
+        .then(data => showData(data.data.tools))
 }
 
 
-const representData = (dataArray) => {
 
-    const firstSixData = dataArray.slice(0, 6);
-    showData(firstSixData);
-}
+
 
 const showData = (dataArray) => {
     const dataContainer = document.getElementById('data-container');
+    console.log(dataArray);
 
-    
     dataArray.forEach(data => {
 
         const dataDiv = document.createElement('div');
-        dataDiv.classList.add('col');
-        dataDiv.classList.add('col-lg-4');
-        // data.classList.add('');
+
+        dataDiv.classList.add('border');
+        dataDiv.classList.add('rounded');
+        dataDiv.classList.add('dataDiv-style');
+
         dataDiv.innerHTML = `
-            <img src= ${data.image} class='img-fluid'>
+            <img src= ${data.image} class="img-fluid p-3 rounded">
+            <div class="p-3">
+                 <h2>Features</h2>
+                    <ol id=list-container-${data.id}>
+
+                    </ol>
+                    
+            </div>
+
         `;
+
+        console.log(dataDiv);
+        console.log(document.getElementById('list-container-01'));
+        console.log(`list-container-${data.id}`);
+
+        for (let i = 0; i < data.features.length; i++) {
+
+            const li = document.createElement('li');
+            li.innerText = `${data.features[i]}`;
+            console.log(li);
+        }
         dataContainer.appendChild(dataDiv);
     })
 }
